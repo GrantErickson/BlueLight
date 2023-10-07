93bcfe5de1ac12d559cf72ed932a7dda0bb74872
Grant Erickson <grant@granterickson.com>
2023-10-07 15:51:59 -0700

    Date mods

--- BlueLight.Web/src/components/EventList.vue
+++ BlueLight.Web/src/components/EventList.vue
@@ -3,7 +3,9 @@
     <v-row>
       <v-col cols="12">
         <v-card v-for="event in events" :key="event.eventId!">
-          <v-card-title> {{ event.name }} on {{ event.date }} </v-card-title>
+          <v-card-title>
+            {{ event.name }} on {{ event.date?.toDateString() }}
+          </v-card-title>
           <v-card-subtitle>
             {{ event.description }}
           </v-card-subtitle>
